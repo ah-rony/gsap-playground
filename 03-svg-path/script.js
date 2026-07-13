286a97c1f29b1d2c6d66c854eaeaf33194e397d4
ah-rony <ahrony1122@gmail.com>
2026-07-13 16:36:52 +0600

    improved svg path animation and responsiveness

--- 03-svg-path/script.js
+++ 03-svg-path/script.js
@@ -1,11 +1,10 @@
-let initialPath = "M 10 250 Q 250 250 1890 250"
-
-let finalPath = "M 10 250 Q 250 250 1890 250"
+let initialPath = "M 10 250 Q 500 250 1890 250"
 
 let string = document.querySelector("#string")
+let svg = document.querySelector("svg");
 
 string.addEventListener("mousemove", function (dets) {
-    initialPath = `M 10 250 Q ${dets.x} ${dets.y} 1890 250`
+    initialPath = `M 10 250 Q ${dets.x} ${dets.y} ${svg.clientWidth - 10} 250`
 
     gsap.to("svg path",{
         attr:{ d: initialPath },
@@ -19,7 +18,9 @@ console.log(document.querySelector("svg path"));
 
 string.addEventListener("mouseleave", function (dets){
     gsap.to("svg path",{
-        attr:{d: finalPath},
+        attr:{
+            d: `M 10 250 Q 250 250 ${svg.clientWidth - 10} 250`
+        },
         duration:1.5,
         ease:"elastic.out(1,0.2)"
     })
